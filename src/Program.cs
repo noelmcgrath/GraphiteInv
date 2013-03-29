7039bd2bc05875f0574275b48ba3f5f68299e3b6
nmcgrath <noel.mcgrath@gmail.com>
2013-03-29 00:07:57 +0000

    Added metric client

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 
 namespace GraphiteInv
@@ -11,6 +12,97 @@ namespace GraphiteInv
 	class Program
 	{
 		static void Main(string[] args)
+		{
+			var _appSettings = ConfigurationManager.AppSettings;
+			var _metricsHostName = _appSettings["metricsHostName"];
+			var _metricsPort = int.Parse(_appSettings["metricsPort"]);
+			var _metricsKeyPrefix = _appSettings["metricsKeyPrefix"];
+
+			var _metricClient = new Metric.Client.Recorder(new Metric.Client.StatsdPipe(_metricsHostName, _metricsPort), _metricsKeyPrefix);
+
+			int _increment = 1;
+			while (true)
+			{
+				System.Threading.Thread.Sleep(100);
+
+				Random r = new Random();
+				var _randomNumber = r.Next(5);
+
+				if (_randomNumber >= 2)
+				{
+					_metricClient.Increment("200");
+				}
+				if (_randomNumber == 3)
+				{
+					_metricClient.Increment("400");
+				}
+				if (_randomNumber == 4)
+				{
+					_metricClient.Increment("500");
+				}
+
+				Console.WriteLine(_increment);
+				_increment++;
+
+				if (_increment % 10 == 0)
+				{
+					Console.WriteLine("{0}.deploy");
+					_metricClient.Increment("deploy");
+				}
+
+				using (var _timer = _metricClient.StartTimer("timer"))
+				{
+					Thread.Sleep(100 + _randomNumber);
+				}
+			}
+		}
+
+		static void MainOld2(string[] args)
+		{
+			var _appSettings = ConfigurationManager.AppSettings;
+			var _metricsHostName = _appSettings["metricsHostName"];
+			var _metricsPort = int.Parse(_appSettings["metricsPort"]);
+			var _metricsKeyPrefix = _appSettings["metricsKeyPrefix"];
+
+			var _statsdClient = new Statsd.StatsdPipe(_metricsHostName, _metricsPort);
+
+			int _increment = 1;
+			while (true)
+			{
+				System.Threading.Thread.Sleep(100);
+
+				Random r = new Random();
+				var _randomNumber = r.Next(15);
+
+				if (_randomNumber >= 2)
+				{
+					_statsdClient.Increment(string.Format("{0}.{1}", _metricsKeyPrefix, 200));
+				}
+				if (_randomNumber == 3)
+				{
+					_statsdClient.Increment(string.Format("{0}.{1}", _metricsKeyPrefix, 400));
+				}
+				if (_randomNumber == 4)
+				{
+					_statsdClient.Increment(string.Format("{0}.{1}", _metricsKeyPrefix, 500));
+				}
+
+				Console.WriteLine(_increment);
+				_increment++;
+
+				if (_increment % 10 == 0)
+				{
+					Console.WriteLine(string.Format("{0}.deploy", _metricsKeyPrefix));
+					_statsdClient.Increment(string.Format("{0}.deploy", _metricsKeyPrefix));
+				}
+
+
+				_statsdClient.Timing(string.Format("{0}.timer", _metricsKeyPrefix), 300 + _randomNumber);
+			}
+		}
+
+
+		static void MainOLD(string[] args)
 		{
 
 			var cpuCounter = new PerformanceCounter();
